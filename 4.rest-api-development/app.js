0883e26d3208503dd23d467df15ee9d2bc6aab06
FurkanYusuf1560 <furkanyusufcevik@gmail.com>
2026-04-19 17:43:23 +0300

    rest api

--- 4.rest-api-development/app.js
+++ 4.rest-api-development/app.js
@@ -15,8 +15,56 @@ let books = [
 ]
 
 app.get('/', (req, res) => {
-
-    req.json({
+    res.json({
         message : "Welcome to our bookstore api"
     })
+})
+
+app.get('/get', (req, res) => {
+    res.json(books);
+})
+
+app.get('/get/:id', (req, res) => {
+    const book = books.find(item => item.id === req.params.id);
+    if(book){
+        res.status(200).json(book)
+    } else {
+        res.status(404).json({
+            message : "Book not found! Please try with a different Book ID"
+        })
+    }
+})
+
+app.post('/add', (req, res) => {
+    const newBook = {
+        id : books.length + 1,
+        title : `Book ${books.length + 1}`
+    }
+
+    books.push(newBook);
+    res.status(200).json({
+        data : newBook,
+        message : "New Book is added successfully"
+    })
+})
+
+app.put('/update/:id', (req, res)=>{
+    const findCurrentBook = books.find(bookItem => bookItem.id === req.params.id);
+    if(findCurrentBook){
+        findCurrentBook.title = req.body.title || findCurrentBook.title
+
+        res.status(200).json({
+            message : `Book with ID ${req.params.id} updated successfully`,
+            data : findCurrentBook
+        })
+    } else {
+        res.status(404).json({
+            message : "Book not found"
+        })
+    }
+})
+
+const PORT = 3000;
+app.listen(PORT, ()=> {
+    console.log(`Server is now running on ${PORT}`);
 })
